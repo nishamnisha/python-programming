1fa2a9fd4882ad08c0fcbf36b79f7698eef7cdc8
nishamnisha <41040371+nishamnisha@users.noreply.github.com>
2018-09-25 15:35:51 +0530

    Add files via upload

--- ainterval.py
+++ ainterval.py
@@ -1,24 +1,12 @@
 lower = 100
 upper = 2000
-
-# To take input from the user
-# lower = int(input("Enter lower range: "))
-# upper = int(input("Enter upper range: "))
-
-for num in range(lower, upper + 1):
-
-   # order of number
-   order = len(str(num))
-    
-   # initialize sum
+for n in range(lower, upper + 1):
+   order = len(str(n))
    sum = 0
-
-   # find the sum of the cube of each digit
-   temp = num
+   temp = n
    while temp > 0:
        digit = temp % 10
        sum += digit ** order
        temp //= 10
-
-   if num == sum:
-       print(num)
+    if n == sum:
+        print(n)
