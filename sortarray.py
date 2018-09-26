7e5c3c479c5bc1794e3092833dc3773c7ed5e462
nishamnisha <41040371+nishamnisha@users.noreply.github.com>
2018-09-26 14:53:51 +0530

    Add files via upload

---
+++ sortarray.py
@@ -0,0 +1,16 @@
+n = int(input("How many figures : ")) 
+storage = []
+result = []
+
+
+for i in range(1,n+1):
+    a = int(input("Enter value" + str(i) + " : "))
+    storage.append(a)
+
+
+for m in range(len(storage)):
+    b = min(storage)
+    storage.remove(b)
+    result.append(b) 
+j = ' '.join(str(i) for i in result)
+print(j)
