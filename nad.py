cd0750fb8598ad0953862cc5dc76b7a68c92c814
nishamnisha <41040371+nishamnisha@users.noreply.github.com>
2018-09-26 14:37:01 +0530

    Add files via upload

---
+++ nad.py
@@ -0,0 +1,11 @@
+  
+def Nth_of_AP(n, a, d) : 
+  
+    return (n + (a - 1) * d) 
+n=2      
+a = 1   
+d = 5  
+    
+print( "The ", N ,"th term of the series is : ", 
+       Nth_of_AP(n, a, d)) 
+  
