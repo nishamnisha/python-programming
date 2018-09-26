99b66953782e369a8fdfcb266790a74c2d32c05e
nishamnisha <41040371+nishamnisha@users.noreply.github.com>
2018-09-26 14:48:45 +0530

    Add files via upload

---
+++ minele.py
@@ -0,0 +1,20 @@
+def Nminelements(list1, N): 
+    final_list =[]; 
+   
+    for i in range(0, N):     
+        min = 9999999; 
+           
+        for j in range(len(list1)):       
+            if list1[j]<min: 
+                min = list1[j]; 
+  
+                 
+        list1.remove(min); 
+        final_list.append(min) 
+           
+    print(final_list) 
+   
+
+list1 = [4, 78, 34, 10, 8, 21, 11, 231]; 
+N = 2; 
+Nminelements(list1, N)
