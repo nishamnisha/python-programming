9f60a9365537fdd75092267162e7c99e1accf3d6
nishamnisha <41040371+nishamnisha@users.noreply.github.com>
2018-12-28 11:28:05 +0530

    Add files via upload

--- prime.py
+++ prime.py
@@ -1,11 +1,14 @@
-num = int(input("Enter any number: "))
-if num > 1:
-    for i in range(2, number):
-        if (num % i) == 0:
-            print(number, "is not a prime number")
-            break
-    else:
-        print(num, "is a prime number")
-else:
-    print(num, "is not a prime number")
+num1=407
+if num1 > 1:
+   
+   for i in range(2,num1):
+       if (num1 % i) == 0:
+           print(num1,"is not a prime number")
+           print(i,"times",num1//i,"is",num1)
+           break
+   else:
+       print(num1,"is a prime number")
+       
 
+else:
+   print(num1,"is not a prime number")
